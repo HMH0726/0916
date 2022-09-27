13151c6958f22ec827e671346858dd5ac9cba433
ORESANJO <eric726july@gmail.com>
2022-09-27 13:36:04 +0800

    deleted:    BMI_CRBFH.js new file:   BMI_CRWFH.js

---
+++ BMI_CRWFH.js
@@ -0,0 +1,24 @@
+//Calculate Recommended Weight From Height.
+//Recommended BMI 18.5 ≤ BMI < 24
+const RLS = require("readline-sync");
+
+var height = RLS.question("Enter Your Height");
+
+
+console.log("Your Height = "+height);
+
+
+var RecommendedWeight_A = 123
+var RecommendedWeight_B = 123
+
+
+
+
+
+console.log("Recommended Weight = "+RecommendedWeight_A+ " ~ " + RecommendedWeight_B);
+
+
+
+//18.5 = ? / (height/100)**2
+
+// BMI = 體重(公斤) / 身高2(公尺2)
