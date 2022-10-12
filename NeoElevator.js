30c2167ce0069af338e7e3c7933a492adbddc47e
黃盟勛 <45138225+HMH0726@users.noreply.github.com>
2022-10-12 15:27:07 +0800

    modified:   NeoElevator.js

--- NeoElevator.js
+++ NeoElevator.js
@@ -2,36 +2,74 @@
 //Elevator
 var RLS = require("readline-sync");
 
-var CurrentFloor=5;
+var CurrentFloor= "3"; //6
 var TargetFloor;
-const TopFloor=6;
-const BottomFloor=-10;
+              //---0----1----2----3---4---5---6---7---8---9--10
+var FloorName = ["B4","B3","B2","B1","1","2","3","4","5","6","7"];
+
+// FloorName.indexOf(TargetFloor.UpperCase)
+
+
+
+//console.log(FloorName.indexOf(CurrentFloor.toUpperCase()));
+
+
 
 do {
-    TargetFloor = RLS.question("Elevator Now At"+ CurrentFloor +" Terget?");
-//TopFloor >= TargetFloor >= BottomFloor
-if ( TargetFloor >= BottomFloor && TargetFloor <= TopFloor ) {
+    TargetFloor = RLS.question("Elevator Now At Floor "+   FloorName[FloorName.indexOf(CurrentFloor.toUpperCase())]   +" Terget?");
+    //TargetFloor = RLS.question("Elevator Now At "+ FloorName[CurrentFloor] +" Terget?");
+
+
+    //console.log(   FloorName.indexOf(TargetFloor.toUpperCase())   );
+
+     if ( FloorName.indexOf(TargetFloor.toUpperCase()) != -1) {
+        //console.log(   FloorName.indexOf(CurrentFloor.toUpperCase())   );
+        console.log("Valid");
+        console.log(FloorName.indexOf(TargetFloor.toUpperCase()) + FloorName[FloorName.indexOf(CurrentFloor.toUpperCase())]);
+        console.log(FloorName.indexOf(TargetFloor.toUpperCase()) - FloorName[FloorName.indexOf(CurrentFloor.toUpperCase())]);
+
+
+
+
+     } else {
+    
+        console.log("Floor" + " < " +TargetFloor + " > " + "Invalid!");
+
+     }
+
+
+
+
+}while(1)
+
+
+
+/*
+
+
+
+if ( TargetFloor >= FloorName.indexOf[0] && TargetFloor <= FloorName.indexOf(TargetFloor.slice(-1).UpperCase) ) {
     console.log("Valid");
 
-    if(TargetFloor < CurrentFloor){
+    if(TargetFloor < FloorName[CurrentFloor]){
         
         //Down
         console.log("Move Down......");
-        while (TargetFloor<CurrentFloor) {
+        while (TargetFloor<FloorName[CurrentFloor]) {
             CurrentFloor--;
-            console.log("Elevator Now At"+ CurrentFloor);
+            console.log("Elevator Now At "+ FloorName[CurrentFloor]);
         }
 
-    }else if(TargetFloor > CurrentFloor){
+    }else if(TargetFloor > FloorName[CurrentFloor]){
         
         //Up
         console.log("Move Up......");
-        while (TargetFloor>CurrentFloor) {
+        while (TargetFloor>FloorName[CurrentFloor]) {
             CurrentFloor++;
-            console.log("Elevator Now At"+ CurrentFloor);
+            console.log("Elevator Now At "+ FloorName[CurrentFloor]);
         }
 
-    }else if(TargetFloor == CurrentFloor){
+    }else if(TargetFloor == FloorName[CurrentFloor]){
         
         //
         console.log("Huh?");
@@ -40,9 +78,15 @@ if ( TargetFloor >= BottomFloor && TargetFloor <= TopFloor ) {
 
 
 } else {
-    console.log("Invalid");
+   
     continue;
 }
 
 
 }while(1)
+
+*/
+
+
+// console.log("Invalid \n");
+// console.log("Please Input Floor Between " + FloorName.toString());
